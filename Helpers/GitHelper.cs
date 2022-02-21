f174dc1f920ce4f9360565512777eac4a93800a4
b3hnia <b3hniya@gmail.com>
2022-02-21 16:43:11 +0000

    AddGitToSolution fixed during commit issue.

--- Helpers/GitHelper.cs
+++ Helpers/GitHelper.cs
@@ -5,16 +5,30 @@ class GitHelper
 {
     public static void AddGitToSolution(PromptModel model)
     {
+        var projectName = model.GetProjectName();
+
         if (model.AddGit == true)
         {
-            BashExec.Run("dotnet new gitignore");
-            BashExec.Run("git init");
-            BashExec.Run("git add *");
-            BashExec.Run("git add .gitignore");
-            BashExec.Run("git commit -m 'init.'");
+            if (projectName == string.Empty)
+            {
+                BashExec.Run("dotnet new gitignore");
+                BashExec.Run("git init");
+                BashExec.Run("git add *");
+                BashExec.Run("git add .gitignore");
+                BashExec.Run("git commit -m 'init.'");
 
 
-            System.Console.WriteLine("Git initialized successfully.");
+                System.Console.WriteLine("Git initialized successfully.");
+            }
+            else
+            {
+                BashExec.Run($"dotnet new gitignore --output {projectName}");
+                BashExec.Run($"git init {projectName}");
+                BashExec.Run($"git -C ./{projectName} add .");
+                BashExec.Run($"git -C ./{projectName} commit -m 'init.'");
+
+                System.Console.WriteLine("Git initialized successfully.");
+            }
         }
     }
 }
