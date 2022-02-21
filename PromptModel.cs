df8983dceb019c32efb457daed1b0ea7fbbe000e
b3hnia <b3hniya@gmail.com>
2022-02-21 20:41:35 +0000

    2 new fields added to PromptModel.
    ProjectName.
    SolutionName.

--- PromptModel.cs
+++ PromptModel.cs
@@ -4,27 +4,32 @@ using Dhl.Options;
 class PromptModel
 {
     //Optional Props
-    private string _projectName = string.Empty;
-    public string GetProjectName() => _projectName;
-    public string SetProjectName(string name) => _projectName = name;
-    public bool IsDifferentLocation() => _projectName != string.Empty;
+    private static string _solutionName = string.Empty;
+    public string GetSolutionName() => _solutionName;
+    public string SetSolutionName(string name) => _solutionName = name;
+
 
     //Required Props
     [Display(Name = "Choose your preferred template")]
     [Required]
     public TemplateTypes Template { get; set; }
 
-    // [Display(Name = "Put solution and project in the same place")]
-    // public bool PutSolutionAndProjectInSamePlace { get; set; }
-
-    // [Display(Name = "Solution Name")]
-    // public string SolutionName;
 
     [Display(Name = "Add git to your project")]
     [Required]
     public bool AddGit { get; set; }
 
+
     [Display(Name = "Add README.md file to your project")]
     [Required]
     public bool AddReadme { get; set; }
+
+
+    [Display(Name = "Put solution and project in the same place")]
+    [Required]
+    public bool PutSolutionAndProjectInSamePlace { get; set; }
+
+    [Display(Name = "Enter project name")]
+    [Required]
+    public string ProjectName { get; set; } = "NewProject";
 }
