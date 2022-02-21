c60e436e1e586067415eaf34d22d4e45d1f838c0
b3hnia <b3hniya@gmail.com>
2022-02-21 15:11:46 +0000

    Project in the start folder added to application as optional choice.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 ﻿using Dhl.Helpers;
 using Sharprompt;
 
+
 var model = Prompt.Bind<PromptModel>();
 
+var projectInThisFolder = Prompt.Confirm("Do you want to initialize your project in this folder");
+if (projectInThisFolder == true)
+{
+    var projectName = Prompt.Input<string>("please Enter project name");
+    model.SetProjectName(projectName);
+}
+
+//Implement add solution and project in a same directory
+
+TemplateHelper.CreateProjectBasedOnTemplate(model);
 GitHelper.AddGitToSolution(model);
--- PromptModel.cs
+++ PromptModel.cs
@@ -1,17 +1,18 @@
 using System.ComponentModel.DataAnnotations;
-using Dhl.Execs;
 using Dhl.Options;
 
 class PromptModel
 {
+    //Optional Props
+    private string _projectName = string.Empty;
+    public string GetProjectName() => _projectName;
+    public string SetProjectName(string name) => _projectName = name;
 
+
+    //Required Props
     [Display(Name = "Choose your preferred template")]
     [Required]
     public TemplateTypes Template { get; set; }
-
-    [Display(Name = "Project name")]
-    [Required]
-    public string ProjectName { get; set; }
 
     // [Display(Name = "Put solution and project in the same place")]
     // public bool PutSolutionAndProjectInSamePlace { get; set; }
