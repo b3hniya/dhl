a45ba1e673097add542320dfb73ccd653af38117
b3hnia <b3hniya@gmail.com>
2022-02-21 16:42:26 +0000

    setting project name bug fixed.

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ using Sharprompt;
 var model = Prompt.Bind<PromptModel>();
 
 var projectInThisFolder = Prompt.Confirm("Do you want to initialize your project in this folder");
-if (projectInThisFolder == true)
+if (projectInThisFolder == false)
 {
     var projectName = Prompt.Input<string>("please Enter project name");
     model.SetProjectName(projectName);
 }
 
 //Implement add solution and project in a same directory
+// var solutionAndProjectInTheSameFolder
 
 TemplateHelper.CreateProjectBasedOnTemplate(model);
 GitHelper.AddGitToSolution(model);
