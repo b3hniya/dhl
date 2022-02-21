96c6a5fc4245b19ba501cb4f7bd3df98c83c1dd6
b3hnia <b3hniya@gmail.com>
2022-02-21 20:41:56 +0000

    GitHelper changed based on PromptModel.

--- Helpers/GitHelper.cs
+++ Helpers/GitHelper.cs
@@ -5,30 +5,23 @@ class GitHelper
 {
     public static void AddGitToSolution(PromptModel model)
     {
-        var projectName = model.GetProjectName();
-
         if (model.AddGit == true)
         {
-            if (model.IsDifferentLocation())
-            {
-                BashExec.Run($"dotnet new gitignore --output {projectName}");
-                BashExec.Run($"git init {projectName}");
-                BashExec.Run($"git -C ./{projectName} add .");
-                BashExec.Run($"git -C ./{projectName} commit -m 'init.'");
-
-                System.Console.WriteLine("Git initialized successfully.");
-            }
+            if (model.PutSolutionAndProjectInSamePlace)
+                InitializeGit(model.ProjectName);
             else
-            {
-                BashExec.Run("dotnet new gitignore");
-                BashExec.Run("git init");
-                BashExec.Run("git add *");
-                BashExec.Run("git add .gitignore");
-                BashExec.Run("git commit -m 'init.'");
-
-
-                System.Console.WriteLine("Git initialized successfully.");
-            }
+                InitializeGit(model.GetSolutionName());
         }
+    }
+
+    private static void InitializeGit(string folderName)
+    {
+
+        BashExec.Run($"dotnet new gitignore --output {folderName}");
+        BashExec.Run($"git init {folderName}");
+        BashExec.Run($"git -C ./{folderName} add .");
+        BashExec.Run($"git -C ./{folderName} commit -m 'init.'");
+
+        System.Console.WriteLine("Git initialized successfully.");
     }
 }
