9d66ce44d961357cb71ce2343092772fe6298137
b3hnia <b3hniya@gmail.com>
2022-03-07 19:26:24 +0330

    Template and FileName field added to AddModel class.
    AddTemplate enum type has new member (New Class)

--- Model/AddModel.cs
+++ Model/AddModel.cs
@@ -1,6 +1,15 @@
-namespace Dhl.Model;
+using System.ComponentModel.DataAnnotations;
+using Dhl.Options;
 
+namespace Dhl.Model;
 public class AddModel
 {
 
+    [Display(Name = "Choose your preferred template")]
+    [Required]
+    public AddTemplateTypes Template { get; set; }
+
+    [Display(Name = "Enter file name")]
+    [Required]
+    public string FileName { get; set; } = "NewClass";
 }
--- Options/AddTemplateTypes.cs
+++ Options/AddTemplateTypes.cs
@@ -1,4 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Dhl.Options;
 public enum AddTemplateTypes
 {
-
+    [Display(Name = "New Class")]
+    Class,
 }
